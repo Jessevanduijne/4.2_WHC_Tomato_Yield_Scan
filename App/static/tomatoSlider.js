b91b9e026179288c2ef862426f9d537442b77305
Rohan van Wijk <rohan_10@live.nl>
2020-06-11 12:31:57 +0200

    Added slider on upload page

---
+++ App/static/tomatoSlider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    console.log('document loaded');
+    $('.tomatoSlider').slick({
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        arrows: false
+    });
+  });
