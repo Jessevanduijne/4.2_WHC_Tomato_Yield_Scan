846f6d456fec66349ab92fb4a6e5c7d3b8ed42ac
Rohan van Wijk <rohan_10@live.nl>
2020-06-18 20:45:02 +0200

    updated model

--- App/WHC_Tomato_Yield_Scanner/config.py
+++ App/WHC_Tomato_Yield_Scanner/config.py
@@ -10,7 +10,7 @@ PERMANENT_SESSION_LIFETIME=3155695200
 ROOT=os.getcwd()
 
 DATABASE=ROOT + "\\db.sqlite"
-MODEL=os.path.join(ROOT, "..\\Model\\") + "tomato_vgg16_4.h5"
+MODEL=os.path.join(ROOT, "..\\Model\\") + "tomato_vgg16_adam_final.h5"
 
 APP_ROOT=os.path.join(ROOT, "WHC_Tomato_Yield_Scanner")
 IMAGE_UPLOADS=os.path.join(APP_ROOT, "static\\uploads\\")
